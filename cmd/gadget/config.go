a8a5db629b0b6de2638b012d0599cb7d0ef67790
computermouth <computermouth@crunchbangplusplus.org>
2017-05-15 11:48:05 -0700

    Added functions for config finding

--- cmd/gadget/config.go
+++ cmd/gadget/config.go
@@ -2,6 +2,11 @@ package main
 
 import (
 	"gopkg.in/yaml.v2"
+    "fmt"
+    "os"
+    "runtime"
+    "path/filepath"
+    "errors"
 )
 
 type GadgetContext struct {
@@ -40,4 +45,45 @@ func NewConfig(config []byte) (GadgetConfig, error) {
 	}
 
 	return g,nil
+}
+
+// helper function for walkup, determines if cwd is '/'
+func isRoot(path string) bool {
+	if runtime.GOOS != "windows" {
+		return path == "/"
+	}
+	switch len(path) {
+	case 1:
+		return os.IsPathSeparator(path[0])
+	case 3:
+		return path[1] == ':' && os.IsPathSeparator(path[2])
+	}
+	return false
+}
+
+// isDriveLetter returns true if path is Windows drive letter (like "c:").
+func isDriveLetter(path string) bool {
+	if runtime.GOOS != "windows" {
+		return false
+	}
+	return len(path) == 2 && path[1] == ':'
+}
+
+// recursive function, returns ("", rc) on failure
+// returns ("/path/to/dir", rc) on success 
+func walkUp ( bottom_dir string ) (string, error) {
+	
+	var rc error = nil
+	
+	if _, err := os.Stat(fmt.Sprintf("%s/main.go", bottom_dir)); err != nil {
+		
+		// haven't found it
+		if isRoot(bottom_dir) || isDriveLetter(bottom_dir) {
+			return "", errors.New("config: could not find configuration file")
+		} else {
+			bottom_dir, rc = walkUp(filepath.Dir(bottom_dir))
+		}
+	}
+	
+	return bottom_dir, rc
 }
