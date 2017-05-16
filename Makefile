b9a93e4f10c5323cb6d7ace9703b82c1e29692a7
Wynter Woods <zerotri@gmail.com>
2017-05-16 13:00:51 -0700

    start: simple cmd running over ssh
    Signed-off-by: Wynter Woods <zerotri@gmail.com>

--- Makefile
+++ Makefile
@@ -2,10 +2,11 @@ SOURCES=\
 	cmd/gadget/main.go \
 	cmd/gadget/config.go \
 	cmd/gadget/build.go \
+	cmd/gadget/init.go \
 	cmd/gadget/start.go
 
 gadget: $(SOURCES)
-	go build -v ./cmd/gadget
+	go build -ldflags="-s -w" -v ./cmd/gadget
 
 test: gadget
 	./gadget -C tests build
---
+++ cmd/gadget/start.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	//"flag"
+	"fmt"
+	"os"
+	//"os/exec"
+	//"bufio"
+	//"strings"
+	"golang.org/x/crypto/ssh"
+	"golang.org/x/crypto/ssh/terminal"
+	//"bytes"
+	"syscall"
+)
+
+
+// Process the build arguments and execute build
+func gadgetStart(args []string, g *GadgetContext) {
+	// find docker binary in path
+
+	config := &ssh.ClientConfig{
+		User: os.Getenv("LOGNAME"),
+		Auth: []ssh.AuthMethod{
+			ssh.RetryableAuthMethod(ssh.PasswordCallback(
+				func() (secret string, err error) {
+					fmt.Print("Password:")
+					password,err := terminal.ReadPassword(syscall.Stdin)
+					fmt.Println("")
+					return string(password),err
+				}), 3),
+		},
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+	}
+
+	client, err := ssh.Dial("tcp", "localhost:22", config)
+	if err != nil {
+		panic(err)
+	}
+
+	session, err := client.NewSession()
+	if err != nil {
+		client.Close()
+		panic(err)
+	}
+
+	fmt.Println("Running docker info...")
+	// docker create --name ${name}_${uuid} ${name}_${uuid}-img
+	out, err := session.CombinedOutput("printenv SHELL")
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Print( string(out) )
+}
