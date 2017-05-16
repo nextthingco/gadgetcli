056d1c348819848eefa81acb2e03b026b27ad14a
computermouth <computermouth@crunchbangplusplus.org>
2017-05-15 17:06:52 -0700

    Remove extra garbage from init.go

--- cmd/gadget/init.go
+++ cmd/gadget/init.go
@@ -4,11 +4,8 @@ import (
 	"github.com/satori/go.uuid"
 	"gopkg.in/yaml.v2"
 	"path/filepath"
-	//"flag"
 	"fmt"
 	"os"
-	//~ "os/exec"
-	//~ "bufio"
 	"io/ioutil"
 )
 
@@ -36,50 +33,4 @@ func gadgetInit(args []string, g *GadgetContext) {
 		os.Exit(1)
 	}
 	
-	// find docker binary in path
-	//~ binary, lookErr := exec.LookPath("docker")
-	//~ if lookErr != nil {
-		//~ panic(lookErr)
-	//~ }
-
-	//~ // loop through 'onboot' config and build containers
-	//~ for _,container := range g.Config.Onboot {
-		//~ fmt.Println(" ==> Building:", container.Name)
-
-		//~ cmd := exec.Command(binary,
-			//~ "build",
-			//~ "--tag",
-			//~ fmt.Sprintf("%s_%s-img", container.Name, container.UUID), //"gadget-networkd_112icx9s-img",
-			//~ fmt.Sprintf("%s/%s", g.WorkingDirectory, container.From)) //
-		//~ cmd.Env = os.Environ()
-		
-		//~ stdOutReader, execErr := cmd.StdoutPipe()
-		//~ stdErrReader, execErr := cmd.StderrPipe()
-		//~ outScanner := bufio.NewScanner(stdOutReader)
-		//~ errScanner := bufio.NewScanner(stdErrReader)
-
-		//~ // goroutine to print stdout and stderr
-		//~ go func() {
-			//~ // TODO: goroutine gets launched and never exits.
-			//~ for {
-				//~ // TODO: add a check here to only print stdout if verbose
-				//~ /*if outScanner.Scan() {
-					//~ fmt.Println(string(outScanner.Text()))
-				//~ }*/
-				//~ _ = outScanner.Scan()
-				//~ if errScanner.Scan() {
-					//~ fmt.Println(string(errScanner.Text()))
-				//~ }
-			//~ }
-		//~ }()
-
-		//~ execErr = cmd.Start()
-		//~ if execErr != nil {
-			//~ panic(execErr)
-		//~ }
-		//~ execErr = cmd.Wait()
-		//~ if execErr != nil {
-			//~ panic(execErr)
-		//~ }
-	//~ }
 }
