df6d33fcc382e2611686db59e2f80d96f7f9ac86
Wynter Woods <zerotri@gmail.com>
2017-05-18 17:01:29 -0700

    deploy: decouple deploy function from command. deploy command transfers both onboot and services now.
    Signed-off-by: Wynter Woods <zerotri@gmail.com>

--- cmd/gadget/deploy.go
+++ cmd/gadget/deploy.go
@@ -5,14 +5,67 @@ import (
 	"os"
 	"os/exec"
 	"io"
+	"golang.org/x/crypto/ssh"
+	"strings"
 )
 
-// Process the build arguments and execute build
-func gadgetDeploy(args []string, g *GadgetContext) {
+func deployContainer( client *ssh.Client, container * GadgetContainer, autostart bool) {
 	binary, err := exec.LookPath("docker")
 	if err != nil {
 		panic(err)
 	}
+
+	fmt.Println("==> deploying:", container.ImageAlias)
+	docker := exec.Command(binary, "save", container.ImageAlias)
+
+	session, err := client.NewSession()
+	if err != nil {
+		client.Close()
+		panic(err)
+	}
+
+	// create pipe for local -> remote file transmission
+	pr, pw := io.Pipe()
+
+	docker.Stdout =  pw
+	session.Stdin = pr
+	session.Stdout = os.Stdout
+	session.Stderr = os.Stderr
+	
+	fmt.Println("Starting session")
+	if err := session.Start(`docker load`); err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Starting docker")
+	if err := docker.Start(); err != nil {
+		panic(err)
+	}
+
+
+	go func() {
+		defer pw.Close()
+		fmt.Println("Waiting on docker")
+		if err := docker.Wait(); err != nil {
+			panic(err)
+		}
+	}()
+	
+	session.Wait()
+	fmt.Println("closing session")
+	session.Close()
+
+	if autostart {
+		runRemoteCommand(client, "docker",
+			"create",
+			"--name", container.Alias,
+			"--restart=always",
+			container.ImageAlias,
+			strings.Join(container.Command[:]," "))
+	}
+}
+// Process the build arguments and execute build
+func gadgetDeploy(args []string, g *GadgetContext) {
 
 	loadConfig(g)
 	ensureKeys()
@@ -24,45 +77,9 @@ func gadgetDeploy(args []string, g *GadgetContext) {
 	}
 
 	for _, onboot := range g.Config.Onboot {
-
-		fmt.Println("==> deploying:", onboot.ImageAlias)
-		docker := exec.Command(binary, "save", onboot.ImageAlias)
-
-		session, err := client.NewSession()
-		if err != nil {
-			client.Close()
-			panic(err)
-		}
-
-		// create pipe for local -> remote file transmission
-		pr, pw := io.Pipe()
-
-		docker.Stdout =  pw
-		session.Stdin = pr
-		session.Stdout = os.Stdout
-		session.Stderr = os.Stderr
-		
-		fmt.Println("Starting session")
-		if err := session.Start(`docker load`); err != nil {
-			panic(err)
-		}
-
-		fmt.Println("Starting docker")
-		if err := docker.Start(); err != nil {
-			panic(err)
-		}
-
-
-		go func() {
-			defer pw.Close()
-			fmt.Println("Waiting on docker")
-			if err := docker.Wait(); err != nil {
-				panic(err)
-			}
-		}()
-		
-		session.Wait()
-		fmt.Println("closing session")
-		session.Close()
+		deployContainer(client, &onboot, true)
+	}
+	for _, service := range g.Config.Services {
+		deployContainer(client, &service, false)
 	}
 }
