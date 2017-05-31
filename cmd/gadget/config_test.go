b37c8e255de0273f1c2940348a21be112a993bfe
computermouth <computermouth@crunchbangplusplus.org>
2017-05-30 18:12:35 -0700

    Test CleanConfig

--- cmd/gadget/config_test.go
+++ cmd/gadget/config_test.go
@@ -8,7 +8,7 @@ import (
 	"github.com/satori/go.uuid"
 )
 
-func TestTemplateConfig(t *testing.T){
+func TestTemplateConfig(t *testing.T) {
 	
 	fmt.Println("TestTemplateConfig")
 	
@@ -37,6 +37,40 @@ func TestTemplateConfig(t *testing.T){
 		t.Error("isn't deeply equal, but should have been")
 		fmt.Println("%+v", expectedConfig)
 		fmt.Println("%+v", testConfig)
+	}
+	
+}
+
+func TestCleanConfig(t *testing.T){
+	
+	fmt.Println("TestCleanConfig")
+	
+	initUu1 := uuid.NewV4()
+	initUu2 := uuid.NewV4()
+
+	// TEST 0
+	
+	testContext := GadgetContext {
+		Config : TemplateConfig("thisisthename", fmt.Sprintf("%s", initUu1), fmt.Sprintf("%s", initUu2)),
+	}
+	
+	args := []string{ "onboot", "newonboot" }
+	
+	GadgetAdd(args, &testContext)
+	testContext.Config.Onboot[0].Alias = "justtobesureit'spopulated"
+	CleanConfig(testContext.Config)
+	
+	if testContext.Config.Onboot[0].Alias != "" {
+		t.Error("failed to clean config")
+	}
+	if testContext.Config.Onboot[0].ImageAlias != "" {
+		t.Error("failed to clean config")
+	}
+	if testContext.Config.Onboot[1].Alias != "" {
+		t.Error("failed to clean config")
+	}
+	if testContext.Config.Onboot[1].ImageAlias != "" {
+		t.Error("failed to clean config")
 	}
 	
 }
